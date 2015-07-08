82531f592b2db815a184fa201871851845c894c6
Jonathan Musso <JonathanMusso@gmail.com>
2015-07-07 21:54:35 -0400

    Basic CRUD complete for Vehicles. Added markup for testing purposes only.

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -8,8 +8,32 @@ class VehiclesController < ApplicationController
   end
 
   def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(params.require(:vehicle).permit(:make, :model, :year, :production_date, :engine, :transmission, :trim, :color, :options, :location, :description))
+    if @vehicle.save
+      flash[:notice] = "The Vehicle was added to the Registry."
+      redirect_to @vehicle
+    else
+      flash[:error] = "There was an error saving the Vehicle to the Registry. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+    if @vehicle.update_attributes(params.require(:vehicle).permit(:make, :model, :year, :production_date, :engine, :transmission, :trim, :color, :options, :location, :description))
+      flash[:notice] = "The Vehicle entry was updated."
+      redirect_to @vehicle
+    else
+      flash[:error] = "There was an error updating the Vehicle. Please try again."
+      render :edit
+    end
   end
 end
