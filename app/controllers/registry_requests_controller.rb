5e6b6d3f5b28fedf8fa4f4761ea2f69d7e46cdfd
Jonathan Musso <JonathanMusso@gmail.com>
2015-07-27 11:45:08 -0400

    Basic timestamps added for approving or denying a record.

--- app/controllers/registry_requests_controller.rb
+++ app/controllers/registry_requests_controller.rb
@@ -7,5 +7,26 @@ class RegistryRequestsController < ApplicationController
     @registry_request = RegistryRequest.find(params[:id])
   end
 
+  def approve
+    @approval = RegistryRequest.find(params[:id])
+    
+    if @approval.update_attribute(:approved_at, Time.now)
 
+      flash[:notice] = "The vehicle was approved for the Registry."
+      redirect_to request.referer
+    else
+      flash[:error] = "There was an error approving the vehicle. Please try again."
+    end
+  end
+
+  def deny
+    @denied = RegistryRequest.find(params[:id])
+
+    if @denied.update_attribute(:denied_at, Time.now)
+      flash[:notice] = "The vehicle was denied."
+      redirect_to request.referer
+    else
+      flash[:error] = "There was an error denying the vehicle. Please try again."
+    end
+  end
 end
