b9b5cdf51e457b32c0b88f1e21bbac24b01ba5e1
Jonathan Musso <JonathanMusso@gmail.com>
2015-08-12 11:36:36 -0400

    Add Notes to Registry Request Approval/Denial

--- app/controllers/registry_requests_controller.rb
+++ app/controllers/registry_requests_controller.rb
@@ -7,25 +7,26 @@ class RegistryRequestsController < ApplicationController
     @registry_request = RegistryRequest.find(params[:id])
   end
 
-  def approve
-    @approval = RegistryRequest.find(params[:id])
+  def update
+    @request = RegistryRequest.find(params[:id])
+
+    if params[:commit] == "Approve"
+      @request.approved_at = Time.now
+      flash_message = "The vehicle was approved for the Registry."
+    elsif params[:commit] == "Deny"
+      @request.denied_at = Time.now
+      flash_message = "The vehicle was denied."
+    end
+
+    # @request.notes = params[:registry_request][:notes]
+    @request.notes = params.require(:registry_request).permit(:notes)
     
-    if @approval.update_attribute(:approved_at, Time.now)
-      flash[:notice] = "The vehicle was approved for the Registry."
-      redirect_to request.referer
+    if @request.save
+      flash[:notice] = flash_message
     else
-      flash[:error] = "There was an error approving the vehicle. Please try again."
+      flash[:error] = "There was an error approving the ehicle. Please try again."
     end
-  end
 
-  def deny
-    @denied = RegistryRequest.find(params[:id])
-
-    if @denied.update_attribute(:denied_at, Time.now)
-      flash[:notice] = "The vehicle was denied."
-      redirect_to request.referer
-    else
-      flash[:error] = "There was an error denying the vehicle. Please try again."
-    end
+    redirect_to registry_requests_path
   end
 end
