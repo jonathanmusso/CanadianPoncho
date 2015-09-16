c53cde86e9c2971f50525683a2e320f065fdce09
Jonathan Musso <jonathanmusso@gmail.com>
2015-09-16 15:08:26 -0400

    Created vehicle scope of class methods for filters.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,14 +3,31 @@ class Vehicle < ActiveRecord::Base
 
   has_many :vehicle_images
   has_many :registry_requests
-  
+
   #accepts_nested_attributes_for :vehicle_images
 
   default_scope { order('created_at DESC') }
   scope :approved, -> { joins(:registry_requests).where("registry_requests.approved_at IS NOT NULL") }
   scope :pending, -> { joins(:registry_requests).where("registry_requests.approved_at IS NULL AND registry_requests.denied_at IS NULL") }
   scope :denied, -> { joins(:registry_requests).where("registry_requests.denied_at IS NOT NULL") }
-  
+  scope :filter_by_make, ->(makes) { where(make: makes) }
+  scope :filter_by_year, ->(years) { where(year: year_conversion[years]) }
+
+  def self.makes
+    ["Pontiac", "Acadian", "Beaumont"]
+  end
+
+  def self.year_ranges
+    ["1900-1909", "1910-1919"]
+  end
+
+  def self.year_conversion
+    {
+      "1900-1909" => (1900..1909).to_a,
+      "1910-1919" => (1910..1919).to_a
+    }
+  end
+
   def primary_and_vehicle_images
     primary = primary_image
     rest = vehicle_images.where(primary: false)
