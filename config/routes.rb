be0c4ec16ef18fd471eb3fffbf9e66434cf7ef47
Jonathan Musso <jonathanmusso@gmail.com>
2015-09-16 15:12:20 -0400

    Created filter route for index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update, :show, :index]
+
   resources :vehicles do
+    collection do
+      post 'filter' => 'vehicles#index'
+    end
     member do
       get 're_edit' => 'vehicles#re_edit'
       post 'resubmit' => 'vehicles#resubmit'
       patch 'resubmit' => 'vehicles#resubmit'
     end
   end
+
   resources :vehicle_images, only: [:update, :destroy]
-  resources :registry_requests do 
-    member do 
+
+  resources :registry_requests do
+    member do
       patch 'approve' => 'registry_requests#approve'
       patch 'deny' => 'registry_requests#deny'
     end
