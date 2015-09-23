e947c6dfb092daaf8836d49391cb223890cb6f57
Jonathan Musso <jonathanmusso@gmail.com>
2015-09-23 16:07:58 -0400

    Created helper method for filter checkbox with my mentor.

--- app/helpers/vehicles_helper.rb
+++ app/helpers/vehicles_helper.rb
@@ -1,2 +1,9 @@
 module VehiclesHelper
+  def display_chosen_check_box_tag(current_option, selected, parameter_name)
+    if selected.to_a.include?(current_option)
+      check_box_tag parameter_name, current_option, checked: "checked"
+    else
+      check_box_tag parameter_name, current_option
+    end
+  end
 end
