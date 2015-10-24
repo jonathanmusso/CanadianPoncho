8684e467b6c693e2441812430fdc24bfa88bbece
Jonathan Musso <jonathanmusso@gmail.com>
2015-10-24 15:00:20 -0400

    Created archive method.

--- app/models/registry_request.rb
+++ app/models/registry_request.rb
@@ -4,4 +4,9 @@ class RegistryRequest < ActiveRecord::Base
   scope :pending, -> { where(denied_at: nil, approved_at: nil) }
   # scope :approved, -> { where.not(approved_at: nil) }
   # scope :denied, -> { where.not(denied_at: nil) }
+
+  def archive
+    self.archived_at = Time.now.utc
+    save!
+  end
 end
