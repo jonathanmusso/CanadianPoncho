089049c12161c0cd2e3b635c91fee3df9d04d116
Jonathan Musso <jonathanmusso@gmail.com>
2015-10-26 15:20:45 -0400

    Changed ul to ol.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def create_list(arr)
-    content_tag :ul do
+    content_tag :ol do
       arr.collect do |line|
         content_tag :li, line
       end.join.html_safe
