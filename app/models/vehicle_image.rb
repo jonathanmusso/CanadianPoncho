ef2e9e6142fb264d919159d034c216b516ce3edf
Jonathan Musso <jonathanmusso@gmail.com>
2015-10-28 15:27:50 -0400

    Added carrierwave_backgrounder configurations.

--- app/models/vehicle_image.rb
+++ app/models/vehicle_image.rb
@@ -1,6 +1,8 @@
 class VehicleImage < ActiveRecord::Base
   belongs_to :vehicle
   mount_uploader :image, ImageUploader
+  process_in_background :image
+  store_in_background :image
 
   def set_to_primary_and_save
     VehicleImage.where(vehicle: vehicle).update_all(primary: false)
