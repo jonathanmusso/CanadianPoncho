b45f993d57d2ce259c12ad135595fc99ff215494
Jonathan Musso <jonathanmusso@gmail.com>
2015-10-29 16:00:48 -0400

    Updating fog for heroku caching.

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,4 +1,7 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
   config.fog_credentials = {
     provider:               'AWS',
     aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
